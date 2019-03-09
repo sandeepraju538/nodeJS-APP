ddb275fab2ae7a8fb3694f691756a358349b8457
sandeepraju <sandeepraju538@gmail.com>
2019-03-09 18:13:18 +0530

    routes created for products and orders

--- server/app.js
+++ server/app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
 
-app.use((req, res, next) => {
-    res.status(200).json({
-        message: 'it worked!'
-    });
-});
+app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
+
+// test app with '/'
+// app.use((req, res, next) => {
+//     res.status(200).json({
+//         message: 'it worked!'
+//     });
+// });
 
 module.exports = app;
