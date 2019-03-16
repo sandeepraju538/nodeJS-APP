6fc7f74be30b62c34efa7cc1d20457996c072800
sandeepraju <sandeepraju538@gmail.com>
2019-03-16 21:53:33 +0530

    added nodemon, morgon, body parser, CORS headers

--- server/app.js
+++ server/app.js
@@ -1,11 +1,43 @@
 const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 const app = express();
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    if(req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Methods", "GET, PUT, POST, PATCH, DELETE");
+        return res.status(200).json({});
+    }
+    next();
+});
+
+// routers configuration
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 // test app with '/'
 // app.use((req, res, next) => {
 //     res.status(200).json({
